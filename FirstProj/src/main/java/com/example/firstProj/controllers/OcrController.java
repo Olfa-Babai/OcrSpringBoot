a58def3c85e90a069cdc4d3e5f008326ecf4c6ee
Olfa-Babai <olfa.babai@esprit.tn>
2021-09-21 19:20:50 +0200

    everything works but ocr

--- FirstProj/src/main/java/com/example/firstProj/controllers/OcrController.java
+++ FirstProj/src/main/java/com/example/firstProj/controllers/OcrController.java
@@ -7,6 +7,7 @@ import com.itextpdf.text.DocumentException;
 import net.sourceforge.tess4j.TesseractException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.example.firstProj.donnees.*;
 
 @RestController
 @RequestMapping("/ocr")
+@CrossOrigin
 public class OcrController {
    @Autowired
    private OcrService ocrService;  
